f3f90d1c931a4b4f49727dc56c38f2ff64a0156a
Phil Rawlings <phil.rawlings@outlook.com>
2021-01-24 17:52:11 +0000

    Added comments

--- eeprom/PiEepromTest/PiEepromTest/Program.cs
+++ eeprom/PiEepromTest/PiEepromTest/Program.cs
@@ -13,18 +13,20 @@ namespace PiEepromTest
 
             using (var device = I2cDevice.Create(settings))
             {
-                // Write data
+                // Write data (2 bytes for data address then 4 bytes of data: "DEFG" )
                 device.Write(new byte[] { 0x00, 0x00, 0x44, 0x45, 0x46, 0x47 });
 
-                Thread.Sleep(5);
-
-                device.Write(new byte[] { 0x00, 0x00 });
+                Thread.Sleep(5); // Wait for write to complete - better to use ack polling, but this is a trivial example.
 
                 // Read data
+                // Set read address
+                device.Write(new byte[] { 0x00, 0x00 }); 
+
+                // Read 4 bytes
                 byte[] buffer = new byte[4];
                 device.Read(buffer);
 
-                Console.WriteLine(Encoding.ASCII.GetString(buffer));
+                Console.WriteLine(Encoding.ASCII.GetString(buffer)); // Expect: "DEFG"
             }
         }
     }
